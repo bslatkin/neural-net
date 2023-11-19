c70ba3d84c685f25c5595b7b453c600057736905
Brett Slatkin <brett@haxor.com>
2023-11-18 16:19:25 -0800

    Maybe this is a backprop implementation?

--- neural_net.py
+++ neural_net.py
@@ -1,12 +1,11 @@
+# Attempting to implement https://towardsdatascience.com/math-neural-network-from-scratch-in-python-d6da9f29ce65
+
+import random
 
 
 class Layer:
     def __init__(self):
-        self.input = None
-        self.output = None
-
-    def initialize(self):
-        pass
+        self.last_input = None
 
     def forward(self, input_vector):
         pass
@@ -14,33 +13,44 @@ class Layer:
     def backward(self, output_error, learning_rate):
         pass
 
-    def size(self):
-        pass
-
 
 class FullyConnected(Layer):
-    def __init__(self, size):
-        self.bias_count = size
-        self.biases = None
-        self.weights = None
+    def __init__(self, input_count, output_count):
+        super().__init__()
+        self.input_count = input_count
+        self.output_count = output_count
 
-    def initialize(self, *, weight_initializer=0, bias_initializer=0):
-        self.biases = [bias_initializer] * self.bias_count
+        # One bias for each output
+        self.biases = []
+        for j in range(self.output_count):
+            bias_j = random.uniform(-0.5, 0.5)
+            self.biases.append(bias_j)
 
-        weights_per_bias_count = self.input.size()
-
+        # Rows are number of inputs
+        # Columns are number of outputs
         self.weights = []
 
-        for j in range(self.bias_count):
-            weights_j = [weight_initializer] * weights_per_bias_count
-            self.weights.append(weights_j)
+        for i in range(self.input_count):
+            weights_i = []
+
+            for j in range(self.output_count):
+                weight_ij = random.uniform(-0.5, 0.5)
+                weights_i.append(weight_ij)
+
+            self.weights.append(weights_i)
 
     def forward(self, input_vector):
+        self.last_input = input_vector
+
         output = []
 
-        for bias_j in self.biases:
+        for j in range(self.output_count):
+            bias_j = self.biases[j]
             output_j = bias_j
-            for weight_ij, x_i in zip(self.weights, input_vector):
+
+            for i in range(self.input_count):
+                weight_ij = self.weights[i][j]
+                x_i = input_vector[i]
                 output_j += weight_ij * x_i
 
             output.append(output_j)
@@ -48,9 +58,46 @@ class FullyConnected(Layer):
         return output
 
     def backward(self, output_error, learning_rate):
-        pass
+        # Output error is ∂E/∂Y
+        bias_error = output_error
 
-    def size(self):
-        return size
+        # Same size as the weights matrix
+        # Rows are number of inputs
+        # Columns are number of outputs
+        weights_error = []
+        for i in range(self.input_count):
+            input_i = self.last_input[i]
+            weights_error_i = []
 
+            for j in range(self.output_count):
+                output_error_j = output_error[j]
+                weight_error_ij = output_error_j * input_i
+                weights_error_i.append(weight_error_ij)
+
+            weights_error.append(weights_error_i)
+
+        # Same size as the input
+        input_error = []
+        for i in range(self.input_count):
+            input_error_i = 0
+
+            for j in range(self.output_count):
+                output_error_j = output_error[j]
+                weight_ij = self.weights[i][j]
+                input_error_i += output_error_j * weight_ij
+
+            input_error.append(input_error_i)
+
+        # Update the biases
+        for j in range(self.output_count):
+            bias_error_j = bias_error[j]
+            self.biases[j] -= learning_rate * bias_error_j
+
+        # Update weights
+        for i in range(self.input_count):
+            for j in range(self.output_count):
+                weights_error_ij = weights_error[i][j]
+                self.weights[i][j] -= learning_rate * weights_error_ij
+
+        return input_error
 
